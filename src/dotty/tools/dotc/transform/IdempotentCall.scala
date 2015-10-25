34d7d537be8527bf854bd898b4f43d9abd5225ad
Allan Renucci <allan.renucci@gmail.com>
2015-10-25 23:02:25 +0100

    Attempt: add support for idempotent call with args

--- src/dotty/tools/dotc/transform/IdempotentCall.scala
+++ src/dotty/tools/dotc/transform/IdempotentCall.scala
@@ -4,12 +4,15 @@ import dotty.tools.dotc.ast.Trees._
 import dotty.tools.dotc.ast.tpd
 import dotty.tools.dotc.core.Contexts.Context
 import dotty.tools.dotc.core.Decorators.StringDecorator
-import dotty.tools.dotc.core.Symbols.{NoSymbol, Symbol}
+import dotty.tools.dotc.core.Symbols._
 import dotty.tools.dotc.core._
 import dotty.tools.dotc.transform.TreeTransforms.{MiniPhaseTransform, TransformerInfo}
 
+import scala.collection.mutable.ListBuffer
+
 class IdempotentCall extends MiniPhaseTransform {
 
+  import IdempotentCall._
   import tpd._
 
   override def phaseName: String = "IdempotentCall"
@@ -23,131 +26,160 @@ class IdempotentCall extends MiniPhaseTransform {
     result
   }
 
-  /** Extract idempotent calls in <code>tree</code>
-   *  - lazy val
-   *  - methods annotated with <code>@Idempotent</code>
-   */
-  private def extractIdempotentCall(tree: Tree)(implicit ctx: Context): List[Tree] = tree match {
-    case vd: ValDef => extractIdempotentCall(vd.rhs)
-    case expr if tpd isIdempotentExpr expr=>
-      expr match {
-        // local lazy val or parameterless method on singleton object
-        case _: Ident if tree.symbol is Flags.MethodOrLazy =>
-          List(tree)
-
-        // method on singleton object
-        case Apply(_: Ident, args) =>
-          List(tree)
-
-        // parameterless method/field on this instance
-        case Select(_: This, _) if expr.symbol is Flags.MethodOrLazy =>
-          List(tree)
-
-        // parameterless method/field on object instance
-        case Select(_: Ident, _) if expr.symbol is Flags.MethodOrLazy =>
-          List(tree)
-
-        // method on this instance
-        case Apply(Select(_: This, _), args) =>
-          List(tree)
-
-        // method on object instance
-        case Apply(Select(_: Ident, _), args) =>
-          List(tree)
-
-        case _ => Nil
-
-      }
-    case _ => Nil
-
-  }
+  /** @return Return <code>true</code> if <code>sym</code> references an
+    *          idempotent operation, <code>false</code> otherwise
+    */
+  def isIdempotent(sym: Symbol)(implicit ctx: Context): Boolean =
+    ((sym is Flags.Method) && (sym hasAnnotation defn.IdempotentAnnotationClass)) || (sym is Flags.Lazy)
 
   /** Idempotent call representation
-   *
-   *  @param fun method symbol
-   *  @param qualifier optional qualifier symbol
-   *  @param args optional list of argument symbol
-   */
+    *
+    *  @param fun method symbol
+    *  @param qualifier optional qualifier symbol
+    *  @param args optional list of argument symbol
+    */
   case class Idempotent(fun: Symbol, qualifier: Symbol = NoSymbol, args: List[Symbol] = Nil)
+
+  object Idempotent {
+
+    /** Try to extract an idempotent call from <code>tree</code>
+      *
+      *  @return The idempotent call as an option, <code>None</code>
+      *          if no idempotent call could be extracted
+      */
+    def apply(tree: Tree)(implicit ctx: Context): Option[Idempotent] = tree match {
+      // fun
+      case Ident(_) if isIdempotent(tree.symbol) =>
+        Some(Idempotent(tree.symbol))
+
+      // qual.fun
+      case Select(qual, _) if isIdempotent(tree.symbol) =>
+        if (qual.symbol == NoSymbol) None
+        else Some(Idempotent(tree.symbol, qual.symbol))
+
+      // fun(args)
+      case Apply(id: Ident, args) if isIdempotent(id.symbol) =>
+        val argsSyms = args map (_.symbol)
+        if (argsSyms contains NoSymbol) None
+        else Some(Idempotent(id.symbol, args = argsSyms))
+
+      // qual.fun(args)
+      case Apply(fun@Select(qual, _), args) if isIdempotent(fun.symbol) =>
+        val argsSyms = args map (_.symbol)
+        if ((qual.symbol :: argsSyms) contains NoSymbol) None
+        else Some(Idempotent(fun.symbol, qual.symbol, argsSyms))
+
+      case _ =>
+        None
+    }
+  }
+
 
   /** Transformation on trees which remove redundant idempotent calls */
   class IdempotentCallElimination extends TreeMap {
-    type State = Map[Idempotent, Symbol]
 
-    // Available idempotent calls
-    private var state: State = Map.empty[Idempotent, Symbol]
+    /** Substitutions defined on idempotent calls */
+    private var substs = Map.empty[Idempotent, Symbol]
 
-    override def transform(tree: Tree)(implicit ctx: Context): Tree = tree match {
-      case Ident(_) =>
-        val idem = Idempotent(tree.symbol)
-        (state get idem fold tree) (ref)
+    /** Buffer of extracted idempotent calls */
+    private var treeBuffer = List.empty[ListBuffer[Tree]]
 
-      case sel@Select(This(_) | Ident(_), _) =>
-        val idem = Idempotent(tree.symbol, sel.qualifier.symbol)
-        (state get idem fold tree) (ref)
+    /** Whether or not idempotent calls can be extracted in the current context */
+    private var ctx: OptimizationContext = notOptimizable
 
-      case Block(stats, expr) =>
-        val beforeTransform = state
-        val tStats = transformBlockStats(stats)
-        val tExpr = transform(expr)
-        state = beforeTransform // reset state to its value before transforming statements
-        cpy.Block(tree)(tStats, tExpr)
+    override def transform(tree: Tree)(implicit c: Context): Tree = {
+      val trans = transformIdem(tree)
+      // extract idempotent call in subtrees
+      trans match {
+        case Select(qual, name) if ctx.optimizable =>
+          transformIdem(Select(transform(qual), name))
 
-      //      case Apply(s @ Select(qualifier, name), args) =>
-      //        println(s.symbol)
-      //        println(qualifier.symbol + "\n")
-      //        tree
-      //
-      //      case Apply(id: Ident, args) =>
-      //        println(id.symbol + "\n")
-      //        tree
+        case Apply(id: Ident, args) if ctx.optimizable =>
+          val (pures, rest) = transformArgs(args)
+          transformIdem(Apply(id, pures ::: rest))
 
-      case _ =>
-        super.transform(tree)
-    }
+        case Apply(Select(qual, name), args) if ctx.optimizable =>
+          val tqual = transform(qual)
+          val (pures, rest) = transformArgs(args)
+          transformIdem(Apply(Select(tqual, name), pures ::: rest))
 
-    /** Traverse <code>trees</code> applying transformation, extracting
-     *  idempotent calls, and augmenting <code>state</code> with the
-     *  extracted calls and their substitution.
-     */
-    def transformBlockStats(trees: List[Tree])(implicit ctx: Context): List[Tree] = {
-      def inner(trees: List[Tree]): List[Tree] = trees match {
-        case s :: ss =>
-          val trans = transform(s)
-          val idemCalls = extractIdempotentCall(trans)
+        case Block(stats, expr) =>
+          val savedSubsts = substs
+          val savedCtx = ctx
+          ctx = optimizable
 
-          val vds = idemCalls map { tree =>
-            val holderName = ctx.freshName().toTermName
-            val valDef = SyntheticValDef(holderName, tree)
+          val tStats = transformStats(stats)
 
-            state += (tree match {
-              case id: Ident =>
-                Idempotent(id.symbol)
-              case Apply(id: Ident, args) =>
-                Idempotent(id.symbol, NoSymbol, ???)
-              case sel@Select(th: This, _) =>
-                Idempotent(sel.symbol, th.symbol)
-              case sel@Select(id: Ident, _) =>
-                Idempotent(sel.symbol, id.symbol)
-              case Apply(sel@Select(th: This, _), args) =>
-                Idempotent(sel.symbol, th.symbol, ???)
-              case Apply(sel@Select(id: Ident, _), args) =>
-                Idempotent(sel.symbol, id.symbol, ???)
-            }) -> valDef.symbol
+          treeBuffer ::= ListBuffer.empty
+          val tExpr = transform(expr)
+          val vds = treeBuffer.head.toList
+          treeBuffer = treeBuffer.tail
 
-            valDef
-          }
-
-          // Reapply transformation with augmented state
-          vds ::: (transform(trans) :: inner(ss))
+          substs = savedSubsts
+          ctx = savedCtx
+          transformIdem(Block(tStats ::: vds, tExpr))
 
         case _ =>
-          Nil
+          super.transform(trans)
+      }
+    }
+
+    override def transformStats(trees: List[Tree])(implicit c: Context): List[Tree] = {
+      trees flatMap { stat =>
+        // Prepend newly created vals to the transformed statement
+        treeBuffer ::= ListBuffer.empty
+        val tstat = transform(stat)
+        treeBuffer.head += tstat
+        val res = treeBuffer.head.toList
+        treeBuffer = treeBuffer.tail
+        res
+      }
+    }
+
+    /** If defined, apply a substitution on <code>tree</code>,
+      *  try to extract an idempotent call otherwise
+      */
+    def transformIdem(tree: Tree)(implicit c: Context): Tree =
+      (Idempotent(tree) fold tree) { idem =>
+        substs get idem match {
+          // New idempotent call in optimizable context
+          case None if ctx.optimizable =>
+            val holderName = c.freshName().toTermName
+            val valDef = SyntheticValDef(holderName, tree)
+            substs += idem -> valDef.symbol
+            treeBuffer.head += valDef
+            ref(valDef.symbol)
+
+          // Redundant idempotent call
+          case Some(symb) => ref(symb)
+          case _          => tree
+        }
       }
 
-      flatten(inner(trees))
+    /** Try to extract idempotent calls in <code>args</code> until
+      *  we encounter an impure expression
+      */
+    def transformArgs(args: List[Tree])(implicit c: Context) = {
+      val (pures, rest) = args span isPureExpr
+      val tpures = transform(pures)
+      val saved = ctx
+      ctx = notOptimizable
+      val trest = transform(rest)
+      ctx = saved
+      (tpures, trest)
     }
 
   }
 
+}
+
+object IdempotentCall {
+
+  final class OptimizationContext(val optimizable: Boolean) extends AnyVal
+
+  /** Apply substitutions and try to extract idempotent calls */
+  final val optimizable = new OptimizationContext(true)
+
+  /** Only apply substitutions */
+  final val notOptimizable = new OptimizationContext(false)
 }
