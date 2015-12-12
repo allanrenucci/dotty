52fe722e2c8763193acc1cbe8fbf2fb8560509c0
Allan Renucci <allan.renucci@gmail.com>
2015-12-12 18:32:01 +0100

    Inner functions

--- src/dotty/tools/dotc/Compiler.scala
+++ src/dotty/tools/dotc/Compiler.scala
@@ -51,6 +51,7 @@ class Compiler {
            new LiftTry,
            new ClassOf),
       List(new PatternMatcher,
+      //--
            new ExplicitOuter,
            new ExplicitSelf,
            new CrossCastAnd,
